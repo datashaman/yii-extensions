3c745275752845d58bad0a4ce15e4bcb59a9dd2a
datashaman <marlinf@datashaman.com>
2009-10-07 11:00:14 +0200

    Added JEditable widget

---
+++ jeditable/DsJEditableWidget.php
@@ -0,0 +1,110 @@
+<?php
+class DsJEditableWidget extends CInputWidget
+{
+  // The URL the editable content is saved to
+  public $saveurl;
+
+  // Method to use when submitting edited content.
+  public $method = 'POST';
+
+  // Function is called after form has been submitted.
+  // Callback function receives two parameters.
+  // Value contains submitted form content.
+  // Settings contain all plugin settings.
+  // Inside function this refers to the original element.
+  public $callback;
+
+  // Name of the submitted parameter which contains edited content.
+  public $submitted_name = 'value';
+
+  // Name of the submitted parameter which contains id.
+  public $submitted_id = 'attribute';
+
+  // Extra parameters when submitting content.
+  // Can be either a hash or function returning a hash.
+  public $submitdata;
+
+  // Input type to use. Default input types are text, textarea or select.
+  public $type;
+
+  // Number of rows if using textarea.
+  public $rows;
+
+  // Number of columns if using textarea.
+  public $cols;
+
+  // Height of the input element in pixels.
+  // Can also be set to none.
+  public $height = 'auto';
+
+  // Width of the input element in pixels.
+  // Can also be set to none.
+  public $width = 'auto';
+
+  // Load content of the element from an external URL.
+  public $loadurl;
+
+  // Request type to use when using loadurl.
+  public $loadtype = 'GET';
+
+  // Extra parameters to add to request when using loadurl.
+  public $loaddata;
+
+  // Form data passed as parameter. Can be either a string or function returning a string.
+  public $data;
+
+  public function run()
+  {
+    list($name, $id) = $this->resolveNameID();
+		if(!isset($this->htmlOptions['id']))
+			$this->htmlOptions['id']=$id;
+		if(!isset($this->htmlOptions['name']))
+			$this->htmlOptions['name']=$name;
+
+		$this->registerClientScript();
+
+    if($this->hasModel()) {
+      echo CHtml::tag('div', $this->htmlOptions, CHtml::encode($this->model->{$this->attribute}));
+    } else {
+    }
+  }
+
+  protected function registerClientScript()
+  {
+    $id = empty($this->htmlOptions['id']) ? $this->id : $this->htmlOptions['id'];
+    $saveurl = empty($this->saveurl) ? $_SERVER['REQUEST_URI'] : $this->saveurl;
+		$miOptions=$this->getClientOptions();
+		$options=$miOptions!==array() ? ','.CJavaScript::encode($miOptions) : '';
+		$js="jQuery(\"#{$id}\").editable(\"{$saveurl}\"{$options});";
+
+		$cs=Yii::app()->getClientScript();
+		$cs->registerCoreScript('jquery');
+		$cs->registerScriptFile(Yii::app()->baseUrl.'/js/jquery_jeditable/jquery.jeditable.js');
+		$cs->registerScript('EditableInputElement#'.$id,$js);
+  }
+
+ 	/**
+	 * @return array the options for the text field
+	 */
+	protected function getClientOptions()
+	{
+		$options=array();
+
+    foreach(array('method', 'submitdata', 'type', 'rows', 'cols', 'height', 'width', 'loadurl', 'loadtype', 'loaddata', 'data') as $property) {
+      $this->$property === null or $options[$property]=$this->$property;
+    }
+
+    $this->submitted_id === null or $options['id'] = $this->submitted_id;
+    $this->submitted_name === null or $options['name'] = $this->submitted_name;
+
+		if(is_string($this->callback))
+		{
+			if(strncmp($this->callback,'js:',3))
+				$options['callback']='js:'.$this->callback;
+			else
+				$options['callback']=$this->callback;
+		}
+
+		return $options;
+	}
+}
